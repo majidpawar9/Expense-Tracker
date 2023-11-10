f8964423a181d90cc2d99de6cadda2a92557e9e4
mahek <mahektailor28@gmail.com>
2023-11-10 15:01:31 -0500

    Added Models for Transaction and Category , also added SQl server

---
+++ Models/ApplicationDbcontext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbcontext : DbContext
+    {
+        public ApplicationDbcontext(DbContextOptions options):base(options)
+            {}
+        public DbSet<Transaction> Transactions { get; set; }    
+        public DbSet<Category> Categories { get; set; }
+    }
+}
